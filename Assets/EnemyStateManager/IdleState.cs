944605489ffcd69c480e1a7214ae271eabda2b37
FilipHlavka <113585003+FilipHlavka@users.noreply.github.com>
2025-03-18 19:13:49 +0100

    konec snad

--- Assets/EnemyStateManager/IdleState.cs
+++ Assets/EnemyStateManager/IdleState.cs
@@ -54,7 +54,7 @@ public class IdleState : State
 
     public void ScanForUnits()
     {
-        // enemak bude utocit na nejblizsi jednotku, pokud na nej budou utocit unity s celkovim poctem zivotu veci nez 1 a pul zivotu enemy, enemy zacne zdrhat
+        
         foreach(var m in MovementController.instance.units)
         {
             if(Vector3.Distance(m.transform.position,enemy.transform.position) < enemy.range)
